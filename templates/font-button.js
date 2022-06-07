ebfad62417c574c7e1bf82f06aab963a999f532b
MHENRIST <maxime.henrist@ynov.com>
2022-06-07 17:15:08 +0200

    presque finis les border blanc

--- templates/font-button.js
+++ templates/font-button.js
@@ -2,7 +2,7 @@ function changeColor(color) {
     document.body.style.background = color;
     document.body.style.color = "black"; 
     document.body.style.border = a;
-    a = "border: 2px solid rgb(232, 232, 232);"
+    a = "border: 2px solid "
     // document.body.style.div.CreatePost = box-shadow;
     // "box-shadow" = "15px 10px 10px white"
 }
@@ -10,7 +10,4 @@ function returnChangeColor(color) {
     document.body.style.background = color;
     document.body.style.color = "white";
 }
-
-document.getElementById("fName").className = document.getElementById("fName").className + " error";  //this adds the error class
-
-document.getElementById("fName").className = document.getElementById("fName").className.replace(" error", ""); //this removes the error class
+document.getElementsByClassName('CreatePost').style.borderColor='rgb(232, 232, 232);';
