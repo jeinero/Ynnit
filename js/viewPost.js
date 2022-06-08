31f65e5cd48e009b153fb53ce31439f0b9c28ce9
Grenier Romain <romain.grenier@ynov.com>
2022-06-08 14:55:59 +0200

    view post js

--- js/viewPost.js
+++ js/viewPost.js
@@ -1,16 +1,21 @@
 const url = new URL(window.location.href)
 const id = url.searchParams.get('id')
 // console.log(id)
-const test = document.getElementById('test')
+const viewpost = document.getElementById('viewpost')
 fetch("/apiposts/"+id)
 .then((response) => response.json())
-.then(function viewPost(post) {
-    post.forEach(element => {
-        const viewPost = document.createElement('div')
-        viewPost.classList = 'viewPost'
-        viewPost.innerHTML = element.Title
-        test.appendChild(viewPost)
+.then(data => {
+       console.log(data)
+        const title = document.createElement('div')
+        title.innerHTML = data.Post.Title
+        title.classList = 'title'
+        viewpost.appendChild(title)
 
-    })
+        const content = document.createElement('div')
+        content.innerHTML = data.Post.Content
+        content.classList = 'content'
+        viewpost.appendChild(content)
+
+       
 })
 
