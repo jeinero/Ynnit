6bafe77a2ea3e9f4a8037dea067a9f805f9c10d4
NSEREIR <nacim.sereir@ynov.com>
2022-06-08 16:29:58 +0200

    Modifications majeures Vincent, Romain, Nacim (Maxime après)

--- js/viewPost.js
+++ js/viewPost.js
@@ -7,16 +7,28 @@ fetch("/apiposts/"+id)
 .then(data => {
        console.log(data)
 
+       const divtop = document.createElement('div')
+       divtop.classList = 'divtop'
+       viewpost.appendChild(divtop)
+
+
+
         const title = document.createElement('div')
         title.innerHTML = data.Post.Title
         title.classList = 'title'
-        viewpost.appendChild(title)
+        divtop.appendChild(title)
+        
+       const username = document.createElement('div')
+       username.innerHTML = "by&ensp;" + data.Post.UsersName
+       username.classList = 'username'
+       divtop.appendChild(username)
 
         const content = document.createElement('div')
         content.innerHTML = data.Post.Content
         content.classList = 'content'
         viewpost.appendChild(content)
 
-       
+      
+
 })
 
