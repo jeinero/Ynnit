a38e4f19af430df46533a7eeb98a3817f41918c5
Grenier Romain <romain.grenier@ynov.com>
2022-06-09 17:52:03 +0200

    post commentaire

--- js/viewPost.js
+++ js/viewPost.js
@@ -1,55 +1,75 @@
 const url = new URL(window.location.href)
 const id = url.searchParams.get('id')
-const viewpost = document.getElementById('viewpost')
-fetch("/apiposts/"+id)
-.then((response) => response.json())
-.then(data => {
-
-       const divtop = document.createElement('div')
-       divtop.classList = 'divtop'
-       viewpost.appendChild(divtop)
-
-        const title = document.createElement('div')
-        title.innerHTML = data.Post.Title
-        title.classList = 'title'
-        divtop.appendChild(title)
-        
-       const username = document.createElement('div')
-       username.innerHTML = "by&ensp;" + data.Post.UsersName
-       username.classList = 'username'
-       divtop.appendChild(username)
-
-        const content = document.createElement('div')
-        content.innerHTML = data.Post.Content
-        content.classList = 'content'
-        viewpost.appendChild(content)
-
-})
-
-
 document.getElementById("btn").onclick = function () {
+
+        onClickComment()
+        location.href = "/viewpost?id=" + element.Id
         
-            onClickComment()
 }
 
 function onClickComment() {
         fetch("/comment", {
-            method: "POST",
-            headers: {
-                "content-type": "application/json"
-            },
-            body: JSON.stringify({
-                content: document.getElementById("content").value,
-            })
+                method: "POST",
+                headers: {
+                        "content-type": "application/json"
+                },
+                body: JSON.stringify({
+                        content: document.getElementById("content").value,
+                })
         })
-        .catch((err) => {
-            document.getElementById("error").innerText = err.error
-    
-            })
-        window.location.assign("/");
-    
-    }
-    
+                .catch((err) => {
+                        document.getElementById("error").innerText = err.error
+                })
+      
+}
+
+const viewpost = document.getElementById('viewpost')
+const viewcommentaire = document.getElementById('viewcommentaire')
+fetch("/apiposts/" + id)
+        .then((response) => response.json())
+        .then(data => {
+
+                const divtop = document.createElement('div')
+                divtop.classList = 'divtop'
+                viewpost.appendChild(divtop)
+
+                const title = document.createElement('div')
+                title.innerHTML = data.Post.Title
+                title.classList = 'title'
+                divtop.appendChild(title)
+
+                const username = document.createElement('div')
+                username.innerHTML = "by&ensp;" + data.Post.UsersName
+                username.classList = 'username'
+                divtop.appendChild(username)
+
+                const content = document.createElement('div')
+                content.innerHTML = data.Post.Content
+                content.classList = 'content'
+                viewpost.appendChild(content)
+
+                data.Comments.forEach(element => {
+                        console.log(element)
+                        const divtop = document.createElement('div')
+                        divtop.classList = 'divtop'
+                        viewcommentaire.appendChild(divtop)
+
+                        const content = document.createElement('div')
+                        content.innerHTML = element.Content
+                        content.classList = 'content'
+                        viewcommentaire.appendChild(content)
+                })
+
+
+        })
+
+      
+               
+
+     
+
+
+
 
 
 
