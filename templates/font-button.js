2d8e9db5102170ec8ff5268a5ef7a681252a2b1e
MHENRIST <maxime.henrist@ynov.com>
2022-06-10 17:05:36 +0200

    j'ai fait joinus, signin,community et la je fais communityprint et modifycommunity

--- templates/font-button.js
+++ templates/font-button.js
@@ -2,9 +2,12 @@ function changeColor(color) {
     document.body.style.background = color;
     document.body.style.color = "black";
     document.body.getElementsByClassName('CreatePost')[0].style.borderColor = "black";
+    document.body.getElementsByTagName('textarea')[0].style.borderColor = "black";
+    // document.body.getElementsById('#centerTop')[0].style.borderColor = "black";
 }
 function returnChangeColor(color) { 
     document.body.style.background = color;
     document.body.style.color = "white";
     document.body.getElementsByClassName('CreatePost')[0].style.borderColor = "white";
+    // document.body.getElementsByTagName('textarea')[0].style.borderColor = "white"; 
 }
