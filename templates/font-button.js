53ef42b9a20b0b38a59a03e61bd4aad9b495eef2
MHENRIST <maxime.henrist@ynov.com>
2022-06-14 14:29:57 +0200

    il reste changemail et c finis

--- templates/font-button.js
+++ templates/font-button.js
@@ -4,10 +4,14 @@ function changeColor(color) {
     document.body.getElementsByClassName('CreatePost')[0].style.borderColor = "black";
     document.body.getElementsByTagName('textarea')[0].style.borderColor = "black";
     // document.body.getElementsById('#centerTop')[0].style.borderColor = "black";
+    document.body.getElementsByClassName('box')[0].style.borderColor = "black";
+    document.body.getElementsByClassName('box')[1].style.borderColor = "black";
 }
 function returnChangeColor(color) { 
     document.body.style.background = color;
     document.body.style.color = "white";
     document.body.getElementsByClassName('CreatePost')[0].style.borderColor = "white";
-    // document.body.getElementsByTagName('textarea')[0].style.borderColor = "white"; 
+    // document.body.getElementsByTagName('textarea')[0].style.borderColor = "white";
+    document.body.getElementsByClassName('box')[0].style.borderColor = "white";
+    document.body.getElementsByClassName('box')[1].style.borderColor = "white";
 }
