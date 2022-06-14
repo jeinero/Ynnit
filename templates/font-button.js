875f3d25a05bb330f43bc1e872df3caf3629bad2
MHENRIST <maxime.henrist@ynov.com>
2022-06-14 15:01:26 +0200

    a jour

--- templates/font-button.js
+++ templates/font-button.js
@@ -2,7 +2,7 @@ function changeColor(color) {
     document.body.style.background = color;
     document.body.style.color = "black";
     document.body.getElementsByClassName('CreatePost')[0].style.borderColor = "black";
-    document.body.getElementsByTagName('textarea')[0].style.borderColor = "black";
+    // document.body.getElementsByTagName('textarea')[0].style.borderColor = "black";
     // document.body.getElementsById('#centerTop')[0].style.borderColor = "black";
     document.body.getElementsByClassName('box')[0].style.borderColor = "black";
     document.body.getElementsByClassName('box')[1].style.borderColor = "black";
@@ -11,6 +11,7 @@ function returnChangeColor(color) {
     document.body.style.background = color;
     document.body.style.color = "white";
     document.body.getElementsByClassName('CreatePost')[0].style.borderColor = "white";
+    // document.body.getElementsById('#centerTop')[0].style.borderColor = "white";
     // document.body.getElementsByTagName('textarea')[0].style.borderColor = "white";
     document.body.getElementsByClassName('box')[0].style.borderColor = "white";
     document.body.getElementsByClassName('box')[1].style.borderColor = "white";
